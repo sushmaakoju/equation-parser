9b2b35f559296168dbd166ae6b9b18fa597b05d1
Sushma Anand Akoju <sakoju@andrew.cmu.edu>
2020-10-26 22:30:14 -0400

    update operator, variable (include long names) and invalid patterns (exclude use of sqrt) - test bug fixes

--- src/equation_parser/constants.py
+++ src/equation_parser/constants.py
@@ -2,7 +2,8 @@ from __future__ import absolute_import
 
 import re
 
-VARIABLE_PATTERN = "^[a-zA-Z]+[_]*[1-9]+" #patterns x_1, x_2
+VARIABLE_PATTERN = "^[a-zA-Z]+[_]*[1-9]*[a-zA-Z]*" #patterns x_1, x_2, x1, f, g, delta
 NUMBER_PATTERN = "^[1-9]+[0]*" #2, 22, 20
-OPERATOR_PATTERN = "(\(|\+|\-|\*|\\|\^|\)|\/){1}" #+-*\^
-INVALID_PATTERN = "[\n\t]+"
+#OPERATOR_PATTERN = "(\(|\+|\-|\*|\\|\^|\)|\/){1}" #+-*\^
+OPERATOR_PATTERN = "[\(|\+|\-|\*|\\|\^|\)|\/]{1}" #+-*\^
+INVALID_PATTERN = "(?i)sqrt|[\n\t]"
